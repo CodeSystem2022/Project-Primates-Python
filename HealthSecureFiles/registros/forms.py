f26612cf8bb23167fb761e989e0352b23a8cd35a
Nicolás Sgandurra <nico.sgandurra@gmail.com>
2023-06-26 23:58:50 -0300

    Undo commit 3ffedb4
    Revirtiendo los cambios del commit "3ffedb4ebd813b892d85d771925a9df1da35fa72" por un error en al subida de los archivos
    
    Co-Authored-By: Osman Herrera <104224611+OsmanH25@users.noreply.github.com>

--- HealthSecureFiles/registros/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from .models import Registro #Importamos el modelo  de la clase Registro
-
-class RegistroForm(ModelForm): #aqui se crea la nueva clase RegistroForm
-    class Meta:
-        model = Registro
-        fields = '__all__'
